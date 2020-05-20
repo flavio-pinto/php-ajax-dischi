7836e18c33ab46841646ebe08938a94ab7de4bc1
Flavio Pinto <flaviopnt@hotmail.it>
2020-05-20 13:23:25 +0200

    Setup development environment - Completed

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi</title>
+</head>
+<body>
+    <!-- JAVASCRIPT -->
+    <script src="dist/assets/scripts/main.js"></script>
+</body>
+</html>
