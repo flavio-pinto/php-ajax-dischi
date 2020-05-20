f927db3466557464a19ae0f6b2e73f9adaa46a94
Flavio Pinto <flaviopnt@hotmail.it>
2020-05-20 17:26:14 +0200

    Print albums - completed

--- index.php
+++ index.php
@@ -3,9 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dist/assets/styles/styles.css">
     <title>PHP Dischi</title>
 </head>
 <body>
+    <?php include 'partials-php/main-content.php';?>
+    <header class="header">
+        <div class="header__content">
+            <img class="header__content__logo" src="dist/assets/images/spotify-logo.png" alt="">
+        </div>
+    </header>
+
+    <main class="main">
+        <div class="main__content">
+            <?php foreach($albums as $album) { ?>
+                <div class="main__content__box">
+                    <img src="<?php echo $album['cover'] ?>" alt="">
+                    <h3 class="main__content__box__title"><?php echo $album['title'] ?></h3>
+                    <h4 class="main__content__box__artist"><?php echo $album['artist'] ?></h4>
+                    <span class="main__content__box__year"><?php echo $album['year'] ?></span>
+                </div>
+            <?php } ?>
+        </div>
+    </main>
     <!-- JAVASCRIPT -->
     <script src="dist/assets/scripts/main.js"></script>
 </body>
---
+++ partials-php/main-content.php
@@ -0,0 +1,107 @@
+<?php
+$albums = [
+    [
+        'cover' => 'dist/assets/images/chvrches.jpg',
+        'title' => 'Love Is Dead',
+        'artist' => 'Chvrches',
+        'year' => '2018'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/65daysofstatic.jpg',
+        'title' => 'The Fall of Math',
+        'artist' => '65daysofstatic',
+        'year' => '2004'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/bad-religion.jpg',
+        'title' => 'Recipe for Hate',
+        'artist' => 'Bad Religion',
+        'year' => '1993'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/bark-psychosis.jpg',
+        'title' => 'Hex',
+        'artist' => 'Bark Psychosis',
+        'year' => '1994'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/converge.jpg',
+        'title' => 'Axe to Fall',
+        'artist' => 'Converge',
+        'year' => '2009'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/explosions-in-the-sky.jpg',
+        'title' => 'The Earth is Not a Cold Dead Place',
+        'artist' => 'Explosions in the Sky',
+        'year' => '2003'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/chvrches.jpg',
+        'title' => 'Love Is Dead',
+        'artist' => 'Chvrches',
+        'year' => '2018'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/genesis.jpg',
+        'title' => 'Selling England By The Pound',
+        'artist' => 'Genesis',
+        'year' => '1973'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/green-carnation.jpg',
+        'title' => 'Light of Day, Day of Darkness',
+        'artist' => 'Green Carnation',
+        'year' => '2001'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/gybe.jpg',
+        'title' => 'Lift Your Skinny Fists Like Antennas to Heaven',
+        'artist' => 'Godspeed You! Black Emperor',
+        'year' => '2000'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/red-house-painters.jpg',
+        'title' => 'Down Colorful Hill',
+        'artist' => 'Red House Painters',
+        'year' => '1992'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/slint.jpg',
+        'title' => 'Spiderland',
+        'artist' => 'Slint',
+        'year' => '1991'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/the-smiths.jpg',
+        'title' => 'The Queen Is Dead',
+        'artist' => 'The Smiths',
+        'year' => '1986'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/the-velvet-underground.jpg',
+        'title' => 'The Velvet Underground & Nico',
+        'artist' => 'The Velvet Underground',
+        'year' => '1967'
+    ],
+
+    [
+        'cover' => 'dist/assets/images/tortoise.jpg',
+        'title' => 'TNT',
+        'artist' => 'Tortoise',
+        'year' => '1998'
+    ],
+];
